2f6d911838abfeb0c6ac92c0bd47dfd208ad0612
Preums <preums@gmx.com>
2018-03-28 11:47:28 +0200

    accueil and node install upgrade

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 var express = require('express');
 var app = express();
 
+
+app.use('/views', express.static('views'));
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -18,5 +21,10 @@ app.get('/admin', function(req, res) {
     res.render('pages/admin');
 });
 
+// acceuil page 
+app.get('/acceuil', function(req, res) {
+    res.render('pages/acceuil');
+    
+});
 app.listen(3000);
-console.log('3000 is the magic port');
+console.log('3000 have to be changed in 8080 for prod');
