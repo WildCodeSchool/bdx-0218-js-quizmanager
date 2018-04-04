55b01485f1b1c775d316fc42ce3047f45de8f990
Cedric <cedric.seng@gmail.com>
2018-04-04 16:48:17 +0200

    Rajout de la navbar

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ app.get('/faq', function(req, res) {
 });
 
 // acceuil page
-app.get('/acceuil', function(req, res) {
-    res.render('pages/acceuil');
-  
+app.get('/accueil', function(req, res) {
+    res.render('pages/accueil');
+
 });
 app.listen(3000);
 console.log('3000 have to be changed in 8080 for prod');
