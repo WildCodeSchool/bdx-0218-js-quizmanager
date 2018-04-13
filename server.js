cc9d5a13e8455a7cd73ffcc1f92ee1a1cc92661d
Cedric <cedric.seng@gmail.com>
2018-04-13 11:23:46 +0200

    liens actifs et pages creation

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // load the things we need
 var express = require('express');
 var app = express();
-
+var varFloat = "";
 
 app.use('/views', express.static('views'));
 
@@ -12,6 +12,7 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 
 // index page
+
 app.get('/', function(req, res) {
     res.render('pages/index');
 });
@@ -23,7 +24,7 @@ app.get('/admin', function(req, res) {
 
 // FAQ page
 app.get('/faq', function(req, res) {
-    res.render('pages/faq');
+    res.render('pages/faq', {varFloat:"floatt"});
 });
 
 // acceuil page
@@ -31,5 +32,23 @@ app.get('/accueil', function(req, res) {
     res.render('pages/accueil');
 
 });
+
+// CREER page
+app.get('/creer', function(req, res) {
+    res.render('pages/creer', {varFloat:"floatt"});
+});
+
+app.get('/creation', function(req, res) {
+    res.render('pages/creationQuizz', {varFloat:"floatt"});
+});
+
+app.get('/finquizz', function(req, res) {
+    res.render('pages/FinQuizz', {varFloat:"floatt"});
+});
+
+app.get('/bravo', function(req, res) {
+    res.render('pages/FinQuizz', {varFloat:"floatt"});
+});
+
 app.listen(3000);
 console.log('3000 have to be changed in 8080 for prod');
